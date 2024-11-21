c8739a966fc30ef242a4211f60125267aaa63921
Oxana Shu <laskka9@gmail.com>
2024-11-21 18:43:20 +0300

    test long reverse

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,24 @@
 from hexlet_pytest.example import reverse
+from pathlib import Path
 
 def test_reverse():
     assert reverse('Hexlet') == 'telxeH'
 
+
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+
+def get_fixture_path(filename):
+    return Path(__file__).parent / 'fixtures' / filename
+
+def read_file(filename):
+    return get_fixture_path(filename).read_text()
+
+
+def test_reverse_with_fixtures():
+    before_reverse = read_file('before.txt')
+    expected = read_file('result.txt')
+    actual = reverse(before_reverse)
+
+    assert actual == expected
